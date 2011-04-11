776a72dacff1e013b98b9d5c7def877db1939113
ryuring <ryuring@b999cc53-1ef7-40c1-9cee-df0f4ca86b21>
2011-04-11 10:57:27 +0000

    コピーライトの年を最新にした
    git-svn-id: http://svn.e-catchup.jp/basertwitter/trunk@18 b999cc53-1ef7-40c1-9cee-df0f4ca86b21

--- config/init.php
+++ config/init.php
@@ -6,11 +6,11 @@
  * PHP versions 4 and 5
  *
  * Baser :  Basic Creating Support Project <http://basercms.net>
- * Copyright 2008 - 2009, Catchup, Inc.
+ * Copyright 2008 - 2011, Catchup, Inc.
  *								18-1 nagao 1-chome, fukuoka-shi
  *								fukuoka, Japan 814-0123
  *
- * @copyright		Copyright 2008 - 2009, Catchup, Inc.
+ * @copyright		Copyright 2008 - 2011, Catchup, Inc.
  * @link			http://basercms.net BaserCMS Project
  * @package			uploader.config
  * @since			Baser v 0.1.0
