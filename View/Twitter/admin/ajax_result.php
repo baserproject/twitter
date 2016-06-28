37c3698a9173161fe585b5d70a4baced9bf0e441
K.Takahashi <kenzo.taka@gmail.com>
2016-06-28 19:36:22 +0900

    バージョン表記とライセンス表記の修正

---
+++ View/Twitter/admin/ajax_result.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Ajax結果表示用
+ * baserCMS : Based Website Development Project http://basercms.net
+ * Copyright 2008 - 2015, baserCMS Users Community http://sites.google.com/site/baserusers/ 
+ * @copyright Copyright 2008 - 2016, baserCMS Users Community
+ * @link http://basercms.net baserCMS Project
+ * @package Twitter.View
+ * @since baserCMS Twitter v 3.0.2
+ * @license http://basercms.net/license/index.html
+ */
+
+echo $result;
