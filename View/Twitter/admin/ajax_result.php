08a8a8cdcb8c764521349387b4a4ccc3dd56133c
K.Takahashi <kenzo.taka@gmail.com>
2016-06-28 19:38:41 +0900

    ライセンスコメントの改行を調整

--- View/Twitter/admin/ajax_result.php
+++ View/Twitter/admin/ajax_result.php
@@ -1,13 +1,15 @@
 <?php
 /**
  * Ajax結果表示用
- * baserCMS : Based Website Development Project http://basercms.net
- * Copyright 2008 - 2015, baserCMS Users Community http://sites.google.com/site/baserusers/ 
- * @copyright Copyright 2008 - 2016, baserCMS Users Community
- * @link http://basercms.net baserCMS Project
- * @package Twitter.View
- * @since baserCMS Twitter v 3.0.2
- * @license http://basercms.net/license/index.html
+ *
+ * baserCMS :  Based Website Development Project <http://basercms.net>
+ * Copyright 2008 - 2015, baserCMS Users Community <http://sites.google.com/site/baserusers/>
+ *
+ * @copyright       Copyright 2008 - 2016, baserCMS Users Community
+ * @link            http://basercms.net baserCMS Project
+ * @package         Twitter.View
+ * @since           baserCMS Twitter v 3.0.2
+ * @license         http://basercms.net/license/index.html
  */
-
+ 
 echo $result;
